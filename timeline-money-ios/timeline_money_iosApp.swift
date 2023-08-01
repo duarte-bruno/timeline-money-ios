355d46f0067082a865e50761759eaaa5ab6dff0b
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2023-08-01 11:10:00 -0300

    Project created

---
+++ timeline-money-ios/timeline_money_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeline_money_iosApp.swift
+//  timeline-money-ios
+//
+//  Created by Bruno Duarte on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct timeline_money_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
